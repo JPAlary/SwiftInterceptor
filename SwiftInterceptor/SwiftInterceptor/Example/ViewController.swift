0074388d5bad1a54e1ead45727cbb029d85d0315
jp.alary <alary.jeanpierre@gmail.com>
2016-11-16 22:09:14 +0100

    Create clean project

---
+++ SwiftInterceptor/SwiftInterceptor/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftInterceptor
+//
+//  Created by Jean-Pierre Alary on 16/11/2016.
+//  Copyright © 2016 Jean-Pierre Alary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: View life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
